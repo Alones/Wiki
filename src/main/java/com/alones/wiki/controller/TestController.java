f08d7c3fc3eeaa1b37ff44a45f7fc07edd959078
alones <1071916721@qq.com>
2021-08-07 22:35:48 +0800

    Revert "1-9 增加自定义配置项示例"
    This reverts commit f8895e5295d7e12acc551760c4c668ca637ab85d.

--- src/main/java/com/alones/wiki/controller/TestController.java
+++ src/main/java/com/alones/wiki/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.alones.wiki.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${test.hello}")
-    private String testHello;
-
     @RequestMapping("/hello")
     public String hello(){
-        return "hello wordasd!"+testHello;
+        return "hello word!";
     }
 }
