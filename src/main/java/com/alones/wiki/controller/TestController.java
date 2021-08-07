f8895e5295d7e12acc551760c4c668ca637ab85d
alones <1071916721@qq.com>
2021-08-07 22:17:56 +0800

    1-9 增加自定义配置项示例 集成热部署

--- src/main/java/com/alones/wiki/controller/TestController.java
+++ src/main/java/com/alones/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.alones.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "hello word!";
+        return "hello wordasd!"+testHello;
     }
 }
